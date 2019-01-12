f7757dea7ece140d5b7721654645bb052bfd80f9
Matt Coppola <matt.coppola@gmail.com>
2019-01-12 13:11:32 -0600

    tests complete for inventory retrieval and qty subtraction

--- test/test-server.js
+++ test/test-server.js
@@ -23,7 +23,7 @@ function tearDownDb() {
     return mongoose.connection.dropDatabase();
 }
 
-// Generate results data
+// Generate Workorder data
 function generateWorkOrderResults() {
     return {
         username: "faker",
@@ -35,7 +35,7 @@ function generateWorkOrderResults() {
     };
 };
 
-// Seed Results DB
+// Seed Workorder DB
 function seedResultsDb() {
     const seedData = [];
 
@@ -46,50 +46,30 @@ function seedResultsDb() {
     return Workorder.insertMany(seedData);
 }
 
-//TESTS
-// build one at a time, test and comment after complete
+// Generate Inventory data
+function generateInventoryData() {
+  return {
+    partNumber: "mainboard",
+    cost: 200,
+    price: 400,
+    qty: 100
+  };
+};
 
-describe('Workorders API resource', function () {
-    const username = 'testUser';
-    const password = 'testPass';
+// Seed Inventory data
+function seedInventoryDb() {
+  const seedData = [];
+  for (let i = 1; i <= 10; i++) {
+    seedData.push(generateInventoryData());
+  }
 
-    before(function () {
-        return runServer(TEST_DATABASE_URL);
-    });
-
-    after(function () {
-        return closeServer();
-    });
-
-    beforeEach(function () {
-        return seedResultsDb();
-    });
-
-    afterEach(function () {
-        return tearDownDb();
-    });
-
-///////////////////////
-// CREATE A NEW USER //
-///////////////////////
-
-  describe('POST: Create a new user', function () {
-      it('Should create a new user', function () {
-          return chai
-              .request(app)
-              .post('/api/users')
-              .send({
-                  username,
-                  password
-              })
-              .then(res => {
-                  expect(res).to.have.status(200);
-              });
-      });
-  });
-});
+  return Inventory.insertMany(seedData);
+}
 
 
+////////////////////////
+// BASIC ROUTER TESTS //
+////////////////////////
 
 describe('api/users', function() {
 
@@ -113,4 +93,213 @@ describe('api/auth', function() {
          res.should.be.json;
        });
    });
+});
+
+
+/////////////////////
+// WORKORDER TESTS //
+/////////////////////
+
+
+describe('Workorders API resource', function () {
+    const username = 'testUser';
+    const password = 'testPass';
+
+    before(function () {
+        // adding time for tests to resolve without error
+        this.timeout(10000);
+        return runServer(TEST_DATABASE_URL);
+    });
+
+    after(function () {
+      // adding time for tests to resolve without error
+        this.timeout(10000);
+        return closeServer();
+    });
+
+    beforeEach(function () {
+        return seedResultsDb();
+    });
+
+    afterEach(function () {
+        return tearDownDb();
+    });
+
+  ///////////////////////
+  // CREATE A NEW USER //
+  ///////////////////////
+
+  describe('POST: Create a new user', function () {
+      it('Should create a new user', function () {
+          return chai
+              .request(app)
+              .post('/api/users')
+              .send({
+                  username,
+                  password
+              })
+              .then(res => {
+                  expect(res).to.have.status(200);
+              });
+      });
+  });
+
+  describe('GET: Retrieve Workorders', function() {
+    it('Should retrieve all Workorders', function() {
+      let res;
+      const token = jwt.sign({
+              user: {
+                  username
+              }
+          },
+          JWT_SECRET, {
+              algorithm: 'HS256',
+              subject: username,
+              expiresIn: '7d'
+          });
+        return chai
+          .request(app)
+          .get('/api/auth/dashboard')
+          .set('Authorization', `Bearer ${token}`)
+                .then(function (_res) {
+                    res = _res;
+                    expect(res).to.have.status(200);
+          });
+    });
+  });
+
+  describe('DELETE: Remove an existing result from db', function () {
+        it('delete an existing record', function () {
+            const token = jwt.sign({
+                    user: {
+                        username
+                    }
+                },
+                JWT_SECRET, {
+                    algorithm: 'HS256',
+                    subject: username,
+                    expiresIn: '7d'
+                });
+
+            let result;
+
+            return Workorder
+                .findOne()
+                .then(function (resResult) {
+                    result = resResult;
+                    return chai.request(app)
+                        .delete(`/api/auth/workorder/${result.id}`)
+                        .set('Authorization', `Bearer ${token}`)
+                })
+                .then(function (res) {
+                    expect(res).to.have.status(200);
+                });
+        });
+    });
+
+
+
+});
+
+
+/////////////////////
+// INVENTORY TESTS //
+/////////////////////
+
+
+describe('Inventory API resource', function () {
+    const username = 'testUser';
+    const password = 'testPass';
+
+    before(function () {
+        // adding time for tests to resolve without error
+        this.timeout(10000);
+        return runServer(TEST_DATABASE_URL);
+    });
+
+    after(function () {
+      // adding time for tests to resolve without error
+        this.timeout(10000);
+        return closeServer();
+    });
+
+    beforeEach(function () {
+        return seedInventoryDb();
+    });
+
+    afterEach(function () {
+        return tearDownDb();
+    });
+
+  ///////////////////////
+  // CREATE A NEW USER //
+  ///////////////////////
+
+  describe('POST: Create a new user', function () {
+      it('Should create a new user', function () {
+          return chai
+              .request(app)
+              .post('/api/users')
+              .send({
+                  username,
+                  password
+              })
+              .then(res => {
+                  expect(res).to.have.status(200);
+              });
+      });
+  });
+
+  describe('GET: Retrieve Inventory', function() {
+    it('Should retrieve all Inventory', function() {
+      let res;
+      const token = jwt.sign({
+              user: {
+                  username
+              }
+          },
+          JWT_SECRET, {
+              algorithm: 'HS256',
+              subject: username,
+              expiresIn: '7d'
+          });
+        return chai
+          .request(app)
+          .get('/api/auth/inventory')
+          .set('Authorization', `Bearer ${token}`)
+                .then(function (_res) {
+                    res = _res;
+                    expect(res).to.have.status(200);
+          });
+    });
+  });
+
+  describe('DELETE: Remove an existing Inventory qty', function () {
+        it('delete an existing inventory qty', function () {
+            const token = jwt.sign({
+                    user: {
+                        username
+                    }
+                },
+                JWT_SECRET, {
+                    algorithm: 'HS256',
+                    subject: username,
+                    expiresIn: '7d'
+                });
+
+            let result;
+
+            return Inventory
+                .findOne()
+                .then(function (resResult) {
+                    result = resResult;
+                    return chai.request(app)
+                        .delete(`/api/auth/inventory/${result.id}`)
+                        .set('Authorization', `Bearer ${token}`)
+                })
+                .then(function (res) {
+                    expect(res).to.have.status(200);
+                });
+        });
+    });
 });
